4d936e1600d52a521486dc295a05974a083f591c
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-18 16:20:44 +0100

    feat: layout and refresh button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 // Dati
 const listElem = document.getElementById("email-list");
+const refreshBtn = document.getElementById("refresh-btn");
+
+// Refresh Function
+const refresh = () => {
+    listElem.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+            console.log(resp);
+            listElem.innerHTML += `
+            <th scope="row">${i}</th>
+            <td>${resp.data.response}</td>
+            `
+        })
+    }
+};
 
 // Esecuzione Logica
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
         console.log(resp);
-        listElem.innerHTML += `<li>${resp.data.response}</li>`
+        listElem.innerHTML += `
+        <th scope="row">${i}</th>
+        <td>${resp.data.response}</td>
+        `
     })
 }
+
+refreshBtn.addEventListener("click", refresh);
