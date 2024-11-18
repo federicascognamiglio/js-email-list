11c8674c2983f4518d08a86428a44218b3a29db1
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-18 16:31:11 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// Dati
+// DATI
 const listElem = document.getElementById("email-list");
 const refreshBtn = document.getElementById("refresh-btn");
 
-// Refresh Function
-const refresh = () => {
-    listElem.innerHTML = "";
-    for (let i = 1; i <= 10; i++) {
+// FUNCTIONS
+// Print Email Function
+const printEmails = (eamilNum) => {
+    for (let i = 1; i <= eamilNum; i++) {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
             console.log(resp);
             listElem.innerHTML += `
@@ -16,15 +16,12 @@ const refresh = () => {
     }
 };
 
-// Esecuzione Logica
-for (let i = 1; i <= 10; i++) {
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-        console.log(resp);
-        listElem.innerHTML += `
-        <th scope="row">${i}</th>
-        <td>${resp.data.response}</td>
-        `
-    })
-}
+// Refresh Function
+const refresh = () => {
+    listElem.innerHTML = "";
+    printEmails(10);
+};
 
+// ESECUZIONE LOGICA
+printEmails(10);
 refreshBtn.addEventListener("click", refresh);
