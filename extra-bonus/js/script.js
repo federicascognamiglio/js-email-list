b72b1e9641811a62409a63be05fdb441585c4902
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-19 18:04:17 +0100

    feat: promises integration completed

--- extra-bonus/js/script.js
+++ extra-bonus/js/script.js
@@ -1,66 +1,36 @@
 // DATI
 const listElem = document.getElementById("email-list");
 const refreshBtn = document.getElementById("refresh-btn");
-const emailArray = [];
+let emailArray = [];
 
 // FUNZIONI
-// Funzione che genera Email
-const getEmail = () => {
-    new Promise(function (resolve, reject) {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-            emailArray.push(resp.data.response);
-            resolve(resp.data.response)
-        })
-    })
-};
-
-// Funxione che itera altra funzione
-const iterateEmail = () => {
-    new Promise(function(resolve, reject) {
-        for (let i = 0; i < 10; i++) {
-            getEmail();
-        }
-        resolve(emailArray);
-    })
-    console.log(emailArray);
-};
-
 // Funzione che stampa Email
 const printEmail = () => {
-    new Promise(function (resolve, reject) {
-        emailArray.forEach((curItem, index) => {
-            console.log(curItem);
-            listElem.innerHTML += `
+    emailArray.forEach((curItem, index) => {
+        listElem.innerHTML += `
             <th scope="row">${++index}</th>
             <td>${curItem}</td>
-            `
-        });
-        resolve(listElem.innerHTML);
-    })
+            `;
+    });
+};
+
+// Funzione che crea array di promises
+let promises = [];
+const iterateEmail = () => {
+    for (let i = 0; i < 10; i++) {
+        promises.push(axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {emailArray.push(resp.data.response)}))
+    }
+    Promise.all(promises).then(printEmail)
+};
+
+// Refresh Function
+const refresh = () => {
+    listElem.innerHTML = "";
+    emailArray = [];
+    iterateEmail();
 };
 
 // ESECUZIONE LOGICA
 iterateEmail();
-Promise.all([getEmail, iterateEmail]).then(printEmail);
-
-
-
-
-
-
-
-
-
-
-// Funzione che genera 10 Email
-// const getEmail = () => {
-//     new Promise(function(resolve, reject) {
-//         for (let i = 1; i <= 10; i++) {
-//             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-//                 emailArray.push(resp.data.response)
-//                 resolve(emailArray);
-//                 console.log(emailArray);
-//             })
-//         }
-//     });
-// };
+refreshBtn.addEventListener("click", refresh);
